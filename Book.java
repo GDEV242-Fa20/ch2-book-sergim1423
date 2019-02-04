16a066de49318b4d727c787d708be8e747767220
Bill Crosbie <billcrosbie@users.noreply.github.com>
2019-02-04 11:32:41 -0500

    Added accessor for author
    first part of 2.83

--- Book.java
+++ Book.java
@@ -3,8 +3,8 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author Bill Crosbie
+ * @version Feb 4, 2019
  */
 class Book
 {
@@ -23,4 +23,17 @@ class Book
     }
 
     // Add the methods here ...
+    /**
+     * Provide access to the string holding the author's name
+     * @return author
+     */
+    public String getAuthor()
+    {
+        return author;
+    }
+    
+    
+    
+    
+    
 }
